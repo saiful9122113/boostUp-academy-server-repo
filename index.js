4cb62bc3db6a5f23a731c5063e20fc59d358ec83
Md. Saiful Islam <saiful9122113@gmail.com>
2022-10-26 12:59:22 +0600

    course details added here

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express=require('express')
 consst app=express();
-const port=process.env.PORT ||5000;
+const port=process.env.PORT || 5000;
+
+
+const courses=require('./data/courses.json');
 
 app.get('/', (req, res)=>{
     res.send('News API Running');
 });
+
+app.get('/course-name', (req, res)=>{
+    res.send(courses)
+})
 
 app.listen(port, ()=>{
     console.log('Server running on the port',port);
